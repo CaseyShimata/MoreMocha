2bca77a77bccb31bcd129cf3ed4408f763ee94e9
shimatacb <shimatacb@gmail.com>
2019-01-19 15:48:22 -0700

    setting up the project. Beginning the UI.

---
+++ MoreMocha/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MoreMocha
+//
+//  Created by Casey Shimata on 1/19/19.
+//  Copyright © 2019 Casey Shimata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+}
+
