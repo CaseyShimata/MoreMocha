2d631ab7c8aa35fb31b4702b933bc2b8d21d3bb8
shimatacb <shimatacb@gmail.com>
2019-01-19 20:23:13 -0700

    setting up view Layout, Firebase connected. launch screen complete, database and storage complete, app icons complete

---
+++ MoreMocha/SelectCoffeeModule/SelectCoffeeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SelectCoffeeViewController.swift
+//  MoreMocha
+//
+//  Created by Casey Shimata on 1/19/19.
+//  Copyright © 2019 Casey Shimata. All rights reserved.
+//
+
+import UIKit
+
+class SelectCoffeeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = hexStringToUIColor(hex: "#51E58F")
+
+    }
+
+
+}
+
