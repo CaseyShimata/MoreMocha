ecc9774c2cf5b420a8c329e2dd8478280f61de00
shimatacb <shimatacb@gmail.com>
2019-01-23 05:14:40 -0700

    autoresize font too small. scroll still off in ipad landscape. may use device type enumerator to give special layouts for ipads and 5s.

--- MoreMocha/SelectCoffeeModule/SelectCoffeeCollectionViewCell.swift
+++ MoreMocha/SelectCoffeeModule/SelectCoffeeCollectionViewCell.swift
@@ -5,12 +5,11 @@
 //  Created by Casey Shimata on 1/19/19.
 //  Copyright © 2019 Casey Shimata. All rights reserved.
 //
-
 import UIKit
 import Kingfisher
 
 class SelectCoffeeCollectionViewCell: UICollectionViewCell {
-    
+   
     //Mark: - properties
     var name: String?
     var title: String?
@@ -35,13 +34,17 @@ class SelectCoffeeCollectionViewCell: UICollectionViewCell {
         name = selectCoffeeModel?.name
         title = selectCoffeeModel?.title
         bgColor = hexToUIColor(selectCoffeeModel?.backgroundColor ?? "#ffffff")
+        
         guard let imageUrlString = selectCoffeeModel?.imageUrl else {
             cupShadowImage.alpha = 0.0
             coffeeImageView.image = UIImage(named: "No_image_available.jpg")
             return
         }
+        
         coffeeImageView.kf.setImage(with: URL(string: imageUrlString))
     }
+    
+    
     
     //Mark: = UIfunctions
     public func bindParentUIInfo(cellWidth: CGFloat) {
@@ -51,55 +54,86 @@ class SelectCoffeeCollectionViewCell: UICollectionViewCell {
         adjustCustomizeButton()
     }
     
+    
     public func adjustCoffeeImage() {
         coffeeImageView.clipsToBounds = false
         cupWidth.constant = parentWidth!
         cupHeight.constant = parentWidth!
     }
-
+    
+    
+    
     public func adjustcupShadowImage() {
-        let heightAndWidth = parentWidth! * 0.48
+        let heightAndWidth = parentWidth! * 0.40
         cupShadowImage.backgroundColor = UIColor.brown
         cupShadowImage.clipsToBounds = false
         cupShadowImage.layer.cornerRadius = heightAndWidth / 2
         cupShadowImageWidth.constant = heightAndWidth
         cupShadowImageHeight.constant = heightAndWidth
-
+        
+        
         let shadowPath = UIBezierPath(rect: CGRect(x: parentWidth! * 0.05, y: parentWidth! * 0.166, width: parentWidth! * 0.75, height: parentWidth! * 0.75))
         cupShadowImage.layer.masksToBounds =  false
-        cupShadowImage.layer.shadowColor = UIColor.purple.cgColor
+        cupShadowImage.layer.shadowColor = UIColor.black.cgColor
         cupShadowImage.layer.shadowOffset = CGSize(width: 0.5, height: 0.4)
-        cupShadowImage.layer.shadowOpacity =  0.4
-        cupShadowImage.layer.shadowRadius = 55.0
+        cupShadowImage.layer.shadowOpacity =  0.45
+        cupShadowImage.layer.shadowRadius = 60.0
         cupShadowImage.layer.shadowPath = shadowPath.cgPath
     }
+    
+    
     
     public func adjustCustomizeButton() {
         customizeButton.clipsToBounds = false
         customizeButton.titleLabel?.adjustsFontSizeToFitWidth = true
-        customizeWidth.constant = parentWidth! * 0.17
-        customizeHeight.constant = parentWidth! * 0.17
-        customizeButton.layer.cornerRadius = parentWidth! * 0.17 / 2
-    }
 
+        customizeWidth.constant = parentWidth! * 0.26
+        
+        customizeHeight.constant = parentWidth! * 0.26
+        
+        customizeButton.layer.cornerRadius = parentWidth! * 0.26 / 2
+        
+    }
+    
+    
+    
+    
     
     //Mark: = animationFunctions
+    
     public func disappear() {
+        
         customizeButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        
     }
+    
+    
     
     public func appear() {
+        
         UIView.animate(withDuration: 0.25, animations: {
-            print("appear hit")
+            
             self.customizeButton.transform = CGAffineTransform.identity
+            
         })
+        
     }
+    
+    
     
     //Mark: - IBAction
+    
     @IBAction func didSelectCustomizeButton(_ sender: Any) {
+        
         //pop over with text field, static collection view, static tableview and submit button
+        
     }
     
-
+    
+    
+    
+    
+    
     
 }
+
--- MoreMocha/SelectCoffeeModule/SelectCoffeeViewController.swift
+++ MoreMocha/SelectCoffeeModule/SelectCoffeeViewController.swift
@@ -1,174 +1,344 @@
 //
+
 //  SelectCoffeeViewController.swift
+
 //  MoreMocha
-//
-//  Created by Casey Shimata on 1/19/19.
-//  Copyright © 2019 Casey Shimata. All rights reserved.
+
 //
 
+//  Created by Casey Shimata on 1/19/19.
+
+//  Copyright © 2019 Casey Shimata. All rights reserved.
+
+//
+
+
+
 import UIKit
+
 import RxSwift
+
 import Kingfisher
+
+
 
 class SelectCoffeeViewController: UIViewController, UIGestureRecognizerDelegate {
     
+    
+    
     //Mark: - properties
+    
     private let disposeBag = DisposeBag()
+    
     private var selectCoffeeViewModel: SelectCoffeeViewModel?
-    private var currentCenterCell: UICollectionViewCell?
+    
+    private var currentCenterIndex: IndexPath?
+    
+    private var cellSpacing: CGFloat = 0.0
+    
+    private var cellWidth: CGFloat = 0.0
+    
+    private var cellOffset: CGFloat = 0.0
+    
+    
     
     fileprivate var selectCoffeeModel: [SelectCoffeeModel]?
-
+    
+    
+    
     //Mark: - IBOutlets
+    
     @IBOutlet weak var coffeeTitle: UILabel!
+    
     @IBOutlet weak var coffeeDetails: UILabel!
+    
     @IBOutlet weak var selectCoffeeCollectionView: UICollectionView!
+    
     @IBOutlet weak var tabBar: UITabBar!
+    
     @IBOutlet weak var collectionParentHeight: NSLayoutConstraint!
     
+    
+    
     //Mark: - lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        selectCoffeeViewModel = SelectCoffeeViewModel()
-        requestCoffeedata()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        setSizes()
+        
     }
+    
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        selectCoffeeViewModel = SelectCoffeeViewModel()
+        
+        requestCoffeedata()
+        
+    }
+    
     
     
     private func requestCoffeedata() {
+        
         selectCoffeeViewModel?.requestCoffee().subscribe(onNext:{ (selectCoffeeModel) in
             
+            
+            
             self.selectCoffeeModel = selectCoffeeModel
-            self.selectCoffeeCollectionConfigration()
-                
+            
+            self.setUpSwipeGestures()
+            
+            
+            
         }).disposed(by: disposeBag)
+        
     }
     
-    private func selectCoffeeCollectionConfigration() {
-        selectCoffeeCollectionView.reloadData()
-
-        selectCoffeeCollectionView.isScrollEnabled = false
-        
-        if selectCoffeeModel?.isEmpty ?? true {
-            selectCoffeeCollectionView.isUserInteractionEnabled = false
-        }
-        else {
-            selectCoffeeCollectionView.isUserInteractionEnabled = true
-            
-            coffeeTitle.text = selectCoffeeModel![0].title
-            coffeeTitle.setLineSpacing(lineHeightMultiple: 0.72)
-//            view.backgroundColor = hexToUIColor(selectCoffeeModel![0].backgroundColor)
-            setUpSwipeGestures()
-        }
-    }
     
     
     private func setUpSwipeGestures() {
+        
         let leftSwipeGest = UISwipeGestureRecognizer(target: self, action: #selector(swipeToViewNextPrevImage))
+        
         leftSwipeGest.direction = .left
-       
+        
+        
+        
         let rightSwipeGest = UISwipeGestureRecognizer(target: self, action: #selector(swipeToViewNextPrevImage))
+        
         rightSwipeGest.direction = .right
         
+        
+        
         leftSwipeGest.delegate = self
+        
         rightSwipeGest.delegate = self
         
+        
+        
         selectCoffeeCollectionView.addGestureRecognizer(leftSwipeGest)
+        
         selectCoffeeCollectionView.addGestureRecognizer(rightSwipeGest)
+        
+        
+        
+        selectCoffeeCollectionConfigration()
+        
+        
+        
     }
     
-    //Mark: - functions
+    
+    
+    private func selectCoffeeCollectionConfigration() {
+        
+        selectCoffeeCollectionView.reloadData()
+        
+        
+        
+        selectCoffeeCollectionView.isScrollEnabled = false
+        
+        
+        
+        if selectCoffeeModel?.isEmpty ?? true {
+            
+            selectCoffeeCollectionView.isUserInteractionEnabled = false
+            
+        }
+            
+        else {
+            
+            selectCoffeeCollectionView.isUserInteractionEnabled = true
+            
+            
+            
+            coffeeTitle.text = selectCoffeeModel![0].title
+            
+            coffeeTitle.setLineSpacing(lineHeightMultiple: 0.72)
+            
+            view.backgroundColor = hexToUIColor(selectCoffeeModel![0].backgroundColor)
+            
+            setSizes()
+            
+            
+            
+        }
+        
+    }
+    
+    
+    
+    //Mark: - autoTriggeredFunction
+    
+    
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         
+        
+        
         super.viewWillTransition(to: size, with: coordinator)
-        ///still having issues with initial collectionView item
-        ///positioning after orientation change but it isn't getting hung up,
-        ///just not centering or skipping one drink on first swipe
-
+        
+        
+        
         coordinator.animate(alongsideTransition: nil, completion: {_ in
             
-            if self.currentCenterCell != nil {
-                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.05, execute: {
-                    (self.currentCenterCell as! SelectCoffeeCollectionViewCell).appear()
-                })
-            }
-
+            self.setSizes()
+            
         })
+        
     }
-
+    
+    
+    
+    
+    
+    
     
     @objc private func swipeToViewNextPrevImage(sender: UISwipeGestureRecognizer) {
         
+        
+        
         // calculate center visible item against screenSize
+        
         guard let visiblesCenterItemIndex = calculateCenterVisibleItem() else {return}
         
+        
+        
         // convert center item in the visibles array to full collection array
+        
         guard let currentItem = getCurrentCellIndex(visibleIndex: visiblesCenterItemIndex) else {return}
+        
         if sender.direction == .left || sender.direction == .right {
+            
             let addSub = sender.direction == .left ? 1 : -1
+            
             let nextItem = IndexPath(item: currentItem.item + addSub, section: 0)
-
+            
+            
+            
             if nextItem.row < selectCoffeeModel?.count ?? 0 && nextItem.row >= 0 {
                 
+                
+                
                 ///breaks swiping right -- nil (next item ?? but not out of bounds interesting.
+                
                 ///solution: test nextItem for nil cases .. use more safety checks ..
+                
                 ///added guard check
+                
                 guard let currentCell = selectCoffeeCollectionView.cellForItem(at: IndexPath(item: currentItem.item, section: 0)) else {return}
-
+                
+                
+                
                 guard let nextCell = selectCoffeeCollectionView.cellForItem(at: IndexPath(item: nextItem.item, section: 0)) else {return}
                 
-                currentCenterCell = nextCell
-
+                
+                
+                currentCenterIndex = nextItem
+                
+                
+                
                 changeLayout(newCurrentCell: nextCell as! SelectCoffeeCollectionViewCell)
                 
+                
+                
                 ///handle disappear
+                
                 (currentCell as! SelectCoffeeCollectionViewCell).customizeButton.transform = CGAffineTransform(scaleX: 0, y: 0)
                 
+                
+                
                 ///transition to next item
+                
                 self.selectCoffeeCollectionView.scrollToItem(at: nextItem, at: .centeredHorizontally, animated: true)
                 
+                
+                
                 ///handle the grow
+                
                 UIView.animate(withDuration: 0.5, animations: {
+                    
                     (nextCell as! SelectCoffeeCollectionViewCell).customizeButton.transform = CGAffineTransform.identity
+                    
                 })
+                
             }
+            
         }
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    //Mark: - functions
     
     private func calculateCenterVisibleItem() -> IndexPath? {
+        
         for item in selectCoffeeCollectionView.visibleCells {
             
+            
+            
             let theAttributes:UICollectionViewLayoutAttributes! = selectCoffeeCollectionView.layoutAttributesForItem(at: selectCoffeeCollectionView.indexPath(for: item)!)
+            
             let cellFrameInSuperview = selectCoffeeCollectionView.convert(theAttributes.frame, to: selectCoffeeCollectionView.superview).midX
+            
+            
+            
             if abs(cellFrameInSuperview - selectCoffeeCollectionView.center.x) < selectCoffeeCollectionView.frame.width / 3 {
+                
                 return selectCoffeeCollectionView.indexPath(for: item)
+                
             }
+            
         }
+        
         return nil
+        
     }
+    
+    
     
     private func getCurrentCellIndex(visibleIndex: IndexPath) -> IndexPath? {
+        
         let visibleItems = selectCoffeeCollectionView.indexPathsForVisibleItems as NSArray
+        
         let visiblesIdxOfCenterItem = visibleItems.index(of: visibleIndex)
+        
         return visibleItems.object(at: visiblesIdxOfCenterItem) as? IndexPath
+        
     }
+    
+    
     
     private func changeLayout(newCurrentCell: SelectCoffeeCollectionViewCell) {
+        
         coffeeTitle.text = newCurrentCell.title
+        
         coffeeTitle.setLineSpacing(lineHeightMultiple: 0.72)
         
-//        view.backgroundColor = newCurrentCell.bgColor
+        view.backgroundColor = newCurrentCell.bgColor
+        
     }
+    
+    
+    
+    
     
     //Mark: - IBAction
+    
     @IBAction func didFinishSelectingCoffee(_ sender: Any) {
         
-    }
-    
-    @IBAction func didClickCustomize(_ sender: Any) {
+        
         
     }
-    
 }
 
 
@@ -179,53 +349,167 @@ class SelectCoffeeViewController: UIViewController, UIGestureRecognizerDelegate 
 
 
 
+
+
+
+
+
+
+
+
+
+
 //Mark: - CollectionView Handling
+
 extension SelectCoffeeViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
     
+    
+    
+    private func setSizes() {
+        
+        let screenWidth = view.frame.width
+        
+        let minWidth = min(screenWidth, view.frame.height)
+        
+        
+        
+        ///what percent of screen is:
+        
+        ///1/6cup, gap, cup, gap, 1/6 cup
+        
+        /// .05 .225, .45, .225, .05
+        
+        ///&& off set is 1/6 cup + gap
+        
+        ///landscape is
+        
+        ///.068333, .22667, .41, .22667, .068333
+        
+        var sizeMultiplier: CGFloat = 0.41
+        
+        let halfCupSize = minWidth * sizeMultiplier / 2
+        
+        var cellSpacingMultiplier: CGFloat = screenWidth * 0.22667 + halfCupSize
+        
+        var offSet = screenWidth * 0.295 + halfCupSize
+        
+        
+        
+        if UIDevice.current.orientation == .portrait {
+            
+            sizeMultiplier = 0.45
+            
+            cellSpacingMultiplier = screenWidth * 0.225
+            
+            offSet = screenWidth * 0.275
+            
+        }
+        
+        cellWidth = minWidth * sizeMultiplier
+        
+        collectionParentHeight.constant = cellWidth
+        
+        cellSpacing = cellSpacingMultiplier
+        
+        cellOffset = offSet
+        
+        //        coffeeTitle.fitTextToBounds()
+        
+        selectCoffeeCollectionView.collectionViewLayout.invalidateLayout()
+        
+        
+        
+        if self.currentCenterIndex != nil {
+            
+            
+            
+            //scroll to needs to wait until rotation
+            
+            self.selectCoffeeCollectionView.scrollToItem(at: self.currentCenterIndex!, at: .centeredHorizontally, animated: false)
+            
+            
+            
+            //animation needs to wait until scroll finished
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.20, execute: {
+                
+                
+                
+                (self.selectCoffeeCollectionView.cellForItem(at: self.currentCenterIndex!) as! SelectCoffeeCollectionViewCell).customizeButton.transform = CGAffineTransform.identity
+                
+            })
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return view.frame.width * 0.115
+        
+        ///space too wide(big) (pad land) --- possible because pad width ratio bigger ---- only if launched in landscape initially .. same with iphone 5s
+        
+        return cellSpacing
+        
     }
-
+    
+    
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let minWidth = min(view.frame.width, view.frame.height) / 1.75
         
-        collectionParentHeight.constant = minWidth
-        
-        return CGSize(width: minWidth, height: minWidth)
+        return CGSize(width: cellWidth, height: cellWidth)
         
     }
-
+    
+    
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        let cellWidth = min(view.frame.width, view.frame.height) / 1.75
-        let offSet = view.frame.width / 2.0 - cellWidth / 2.0
         
-        return UIEdgeInsets(top: 0, left: offSet, bottom: 0, right: offSet)
+        ///offset too far right(big) pad portrait ---- only if launched in landscape initially .. / same with iphone 5s /// --- considering my calculation for spacing is not accurately dynamic
+        
+        ///.. solution is to consider the math
+        
+        return UIEdgeInsets(top: 0, left: cellOffset, bottom: 0, right: cellOffset)
+        
     }
-
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         return selectCoffeeModel?.count ?? 0
+        
     }
+    
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectCoffeeCell", for: indexPath) as! SelectCoffeeCollectionViewCell
         
-        cell.bindParentUIInfo(cellWidth: min(view.frame.width, view.frame.height) / 1.75)
         
-        handleReloadButtonAnimate(indexPath: indexPath, cell: cell)
+        
+        cell.bindParentUIInfo(cellWidth: cellWidth)
+        
+        
+        
+        indexPath.row == 0 ? cell.appear() : cell.disappear()
+        
+        
         
         if let thisSelectCoffeeItem = selectCoffeeModel?[indexPath.row] {
+            
             cell.bindData(selectCoffeeModel: thisSelectCoffeeItem)
+            
         }
         
+        
+        
         return cell
+        
     }
     
-    
-    
-    private func handleReloadButtonAnimate(indexPath: IndexPath, cell: SelectCoffeeCollectionViewCell) {
-        indexPath.row == 0 && currentCenterCell == nil ? cell.appear() : cell.disappear()
-    }
 }
 
